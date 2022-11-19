cd3cf5a0bb3af45de9ceec3635b63124229e9588
tanselbay1 <tanselbay1@hotmail.com>
2022-11-19 22:25:09 +0700

    Make Hamburger Menu working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,63 @@
+// Hamburger Menu Icons
+import { useState } from 'react';
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
+
 import ThemeToggle from './ThemeToggle';
 
 export default function Header() {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
+    const handleToggle = () => {
+        setNavbarOpen((prevState) => !prevState);
+    };
+
+    const menu = (
+        <ul className="menu">
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#skills">Skills</a>
+            </li>
+            <li>
+                <a href="#projects">Projects</a>
+            </li>
+        </ul>
+    );
+
     return (
         <div className="header__container">
+            <h3 className="logo">
+                NAME <span className="surname">SURNAME</span>
+            </h3>
             <nav>
-                <h3 className="logo">
-                    Name <span>Surname</span>
-                </h3>
-                <ul className="menu">
-                    <li>
-                        <a href="#home">Home</a>
-                    </li>
-                    <li>
-                        <a href="#about">About</a>
-                    </li>
-                    <li>
-                        <a href="#skills">Skills</a>
-                    </li>
-                    <li>
-                        <a href="#projects">Projects</a>
-                    </li>
-                </ul>
-                <ThemeToggle />
+                <button type="button" onClick={handleToggle}>
+                    {navbarOpen ? (
+                        <MdClose
+                            style={{
+                                color: '#fff',
+                                backgroundColor: '#181a1b',
+                                height: '30px',
+                                width: '30px',
+                            }}
+                        />
+                    ) : (
+                        <FiMenu
+                            style={{
+                                color: '#fff',
+                                backgroundColor: '#181a1b',
+                                height: '30px',
+                                width: '30px',
+                            }}
+                        />
+                    )}
+                </button>
+                {navbarOpen && menu}
+                {navbarOpen && <ThemeToggle />}
             </nav>
         </div>
     );
