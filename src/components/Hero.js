9387ccede51b2e3e011c593a9bb89b3da6acaf32
Kai Paterson-Hall <kaizwebdesign@gmail.com>
2022-11-24 23:40:28 +0000

    Install typed.js package and implement in hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,31 @@
+import Typed from 'typed.js';
+import { useEffect, useRef } from 'react';
+
 export default function Hero() {
+    const typedElement = useRef(null);
+    const typedInstance = useRef(null);
+
+    useEffect(() => {
+        const options = {
+            strings: ['Coder', 'Programmer', 'Developer'],
+            startDelay: 300,
+            typeSpeed: 150,
+            backSpeed: 100,
+            loop: true,
+        };
+
+        typedInstance.current = new Typed(typedElement.current, options);
+
+        return () => typedInstance.current.destroy();
+    }, []);
+
     return (
         <div className="hero-container">
             <h1 className="title">
-                Hi, I'm a <span className="animated-text">coder.|</span>
+                Hi, I'm a{' '}
+                <span ref={typedElement} className="animated-text">
+                    Coder
+                </span>
             </h1>
             <h2 className="sub-title">
                 Full-time student with passion for web development
