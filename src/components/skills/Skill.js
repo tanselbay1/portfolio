4672233a09e0d1e2807e6c40891c63684eeed6b6
tanselbay1 <tanselbay1@hotmail.com>
2023-01-02 11:21:21 +0700

    Add styles to Skills Component

--- src/components/skills/Skill.js
+++ src/components/skills/Skill.js
@@ -1,13 +1,18 @@
 import { Icon } from '@iconify/react';
+import { v4 as uniqueID } from 'uuid';
 
 export default function Skill({ header, headerIcon, items }) {
     return (
         <div className="skill">
-            <Icon icon={headerIcon} color="hsl(356, 74%, 60%)" />
+            <Icon
+                icon={headerIcon}
+                className="header-icon"
+                color="hsl(356, 74%, 60%)"
+            />
             <h2 className="skill-header">{header}</h2>
             <ul className="items">
                 {items.map((item) => (
-                    <li>
+                    <li key={uniqueID()}>
                         {item.title}
                         <Icon icon={item.icon} inline="true" />
                     </li>
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,32 +1,34 @@
 import SectionHeader from '../UI/SectionHeader';
 import Skill from './Skill';
 
-const languagesIcon = 'heroicons:code-bracket';
+// Iconify icons for Headers
+const languagesIcon = 'mingcute:code-fill';
+
 const librariesFrameworksIcon = 'fa-solid:book-reader';
 const technologiesIcon = 'mdi:tools';
 
 const languages = [
-    { title: 'javascript', icon: 'logos:javascript' },
-    { title: 'html', icon: 'vscode-icons:file-type-html' },
-    { title: 'css', icon: 'vscode-icons:file-type-css' },
+    { title: 'Javascript', icon: 'logos:javascript' },
+    { title: 'Html', icon: 'vscode-icons:file-type-html' },
+    { title: 'Css', icon: 'vscode-icons:file-type-css' },
 ];
 
 const librariesFrameworks = [
-    { title: 'react', icon: 'vscode-icons:file-type-reactjs' },
-    { title: 'sass', icon: 'vscode-icons:file-type-sass' },
-    { title: 'framer-motion', icon: 'ph:framer-logo-duotone' },
+    { title: 'React', icon: 'vscode-icons:file-type-reactjs' },
+    { title: 'Sass', icon: 'vscode-icons:file-type-sass' },
+    { title: 'Framer-Motion', icon: 'ph:framer-logo-duotone' },
 ];
 
 const technologies = [
-    { title: 'git', icon: 'mdi:git' },
-    { title: 'webpack', icon: 'logos:webpack' },
-    { title: 'netlify', icon: 'vscode-icons:file-type-netlify' },
-    { title: 'trello', icon: 'logos:trello' },
+    { title: 'Git', icon: 'mdi:git' },
+    { title: 'Webpack', icon: 'logos:webpack' },
+    { title: 'Netlify', icon: 'vscode-icons:file-type-netlify' },
+    { title: 'Trello', icon: 'logos:trello' },
 ];
 
 export default function Skills() {
     return (
-        <section id="section">
+        <section id="skills">
             <div className="container">
                 <div className="header">
                     <SectionHeader title="My" span="Skills" />
